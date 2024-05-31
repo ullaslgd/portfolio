56b5a4a9b91018673ceec69419cad984316f0976
Ullas <clubullas@gmail.com>
2024-05-31 19:02:02 +0530

    tooltip cover

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import StackIcon from "tech-stack-icons";
 import { tech } from '../constants';
 import Tooltip from '@mui/material/Tooltip';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import { Button } from '@mui/material';
+ 
 
 const Skills = () => {
   return (
@@ -14,7 +17,7 @@ const Skills = () => {
        
       {tech.map((icon)=>(
         <div className="bg-[#0a0a0a] backdrop-blur-xl rounded-[20px] p-[20px] max-sm:p-[15px] border-[1px] border-teal-800 flex justify-center items-center shadow-xl">
-          <Tooltip title={icon.tip} arrow > <img src={icon.name} className="w-[50px] max-sm:w-[30px]"/> </Tooltip>
+          <Tooltip disableFocusListener title={icon.tip} arrow > <Button> <img src={icon.name} className="w-[50px] max-sm:w-[30px]"/> </Button> </Tooltip>
         </div>
       ))}
       
