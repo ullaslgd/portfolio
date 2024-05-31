ebb9803eef139b82c68999c58073f9c602b68be8
Ullas <clubullas@gmail.com>
2024-05-31 18:16:47 +0530

    added tooltip

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import StackIcon from "tech-stack-icons";
 import { tech } from '../constants';
+import Tooltip from '@mui/material/Tooltip';
 
 const Skills = () => {
   return (
@@ -13,7 +14,7 @@ const Skills = () => {
        
       {tech.map((icon)=>(
         <div className="bg-[#0a0a0a] backdrop-blur-xl rounded-[20px] p-[20px] max-sm:p-[15px] border-[1px] border-teal-800 flex justify-center items-center shadow-xl">
-          <img src={icon.name} className="w-[50px] max-sm:w-[30px]"/>
+          <Tooltip title={icon.tip} arrow > <img src={icon.name} className="w-[50px] max-sm:w-[30px]"/> </Tooltip>
         </div>
       ))}
       
